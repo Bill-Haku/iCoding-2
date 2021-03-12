e257ca65138dde4e73655778eb3fd25cf2565fe8
HakuBill <battleandwar@hotmail.com>
2021-03-12 09:34:52 +0800

    add LnkList (not finished)

--- LnkList - 1.c
+++ LnkList - 1.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #define ElemType int
+#define LnkList LinkList
 
 struct _lnklist{
     ElemType data;
@@ -17,14 +18,14 @@ void lnk_del_x2y(LinkList L, ElemType mink, ElemType maxk) {
 	last = L;
 	int flag = 0;
 	while(pre != NULL) {
-		if (pre->data >= mink) {
+		if (pre->data <= maxk) {
 			flag = 1;
 		}
 		pre = pre->next;
 		if (!flag) {
 			last = last->next;
 		}
-		if(pre->data >= maxk) {
+		if(pre->data <= mink) {
 			flag = 0;
 			last->next = pre;
 			break;
@@ -33,6 +34,29 @@ void lnk_del_x2y(LinkList L, ElemType mink, ElemType maxk) {
 }
 
 int main() {
-
+	freopen("LnkList - 1.in","r",stdin);
+	LnkList *L;
+	*L = (LnkList)malloc(sizeof(Node));
+	(*L)->next = NULL;
+	int input;
+	LnkList *p;
+	p = L;
+	Node *r;
+	int i = 0;
+	int mink, maxk;
+	scanf("%d%d", &mink, &maxk);	
+	while(scanf("%d", &input) != EOF) {
+		r = (Node*)malloc(sizeof(Node));
+		r->data = input;
+		p->next = r;
+		p = r;
+	}
+	p = NULL;
+	lnk_del_x2y(L,mink,maxk);
+	p = L->next;
+	while(p->next != NULL) {
+		printf("%d ", p->data);
+		p = p->next;
+	}
 	return 0;
 }
