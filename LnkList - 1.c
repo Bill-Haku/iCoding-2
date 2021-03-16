6e5785b60ea2ecb1067096663399ead983a62ffb
HakuBill <battleandwar@hotmail.com>
2021-03-16 14:43:31 +0800

    finish LnkList - 1

--- LnkList - 1.c
+++ LnkList - 1.c
@@ -2,15 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 #define ElemType int
-#define LnkList LinkList
 
-struct _lnklist{
+struct _LinkList{
     ElemType data;
-    struct _lnklist *next;
+    struct _LinkList *next;
 };
-typedef struct _lnklist Node;
-typedef struct _lnklist *LinkList;
-
+typedef struct _LinkList Node;
+typedef struct _LinkList *LinkList;
+int mink, maxk;
 
 void lnk_del_x2y(LinkList L, ElemType mink, ElemType maxk) {
 	Node *pre, *last;
@@ -18,45 +17,66 @@ void lnk_del_x2y(LinkList L, ElemType mink, ElemType maxk) {
 	last = L;
 	int flag = 0;
 	while(pre != NULL) {
-		if (pre->data <= maxk) {
-			flag = 1;
-		}
-		pre = pre->next;
-		if (!flag) {
-			last = last->next;
-		}
-		if(pre->data <= mink) {
+		if((pre->data >= maxk || pre->data <= mink) && !flag) {
+			last = pre;
+			pre = pre->next;
 			flag = 0;
+			continue;
+		}
+		if(pre->data < maxk && pre->data > mink) {
+			last->next = pre->next;
+			free(pre);
+			pre = last->next;
+			flag = 1;
+			continue;
+		}
+		if((pre->data >= maxk || pre->data <= mink) && flag) {
 			last->next = pre;
 			break;
 		}
 	}
 }
 
+void initList(LinkList *L) {
+	*L = (LinkList)malloc(sizeof(Node));
+	(*L)->next = NULL;
+}
+
+void createFromTail(LinkList L) {
+	Node *r;
+	r = L;
+	Node *s;
+	int i = 0;
+	int flag = 1;
+	int input;
+	
+	while(scanf("%d", &input) != EOF) {
+		s = (Node*)malloc(sizeof(Node));
+		s->data = input;
+		r->next = r;
+		r = s;
+	}
+	r->next = NULL;
+}
+
+void outputList(LinkList L) {
+	Node *r;
+	r = L->next;
+	while(r->next != NULL) {
+		printf("%d ", r->data);
+		r = r->next;
+	}
+	printf("\n");
+}
+
 int main() {
 	freopen("LnkList - 1.in","r",stdin);
-	LnkList *L;
-	*L = (LnkList)malloc(sizeof(Node));
-	(*L)->next = NULL;
-	int input;
-	LnkList *p;
-	p = L;
-	Node *r;
-	int i = 0;
-	int mink, maxk;
-	scanf("%d%d", &mink, &maxk);	
-	while(scanf("%d", &input) != EOF) {
-		r = (Node*)malloc(sizeof(Node));
-		r->data = input;
-		p->next = r;
-		p = r;
-	}
-	p = NULL;
-	lnk_del_x2y(L,mink,maxk);
-	p = L->next;
-	while(p->next != NULL) {
-		printf("%d ", p->data);
-		p = p->next;
-	}
+	LinkList *L;
+	initList(L);
+	scanf("%d%d", &mink, &maxk);
+	createFromTail(*L);
+	lnk_del_x2y(*L,mink,maxk);
+	printf("%d %d\n", mink, maxk);
+	//outputList(*L);
 	return 0;
 }
